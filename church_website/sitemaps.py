5c662d68da127710541c0f0dcda6758f1556885d
qwame2 <papaqamwe25@gmail.com>
2026-03-10 17:55:22 +0000

    Update

---
+++ church_website/sitemaps.py
@@ -0,0 +1,29 @@
+# sitemaps.py
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+
+    def items(self):
+        # Return a list of URL names for your static views
+        return [
+            'home', 
+            'church_news_archive', 
+            'youth_church_landing', 
+            'gallery', 
+            'map', 
+            'menfellow', 
+            'wemenfellow', 
+            'youth', 
+            'youth_activities',
+            'book', 
+            'child', 
+            'reading_page', 
+            'podcast', 
+            'game',
+        ]
+
+    def location(self, item):
+        return reverse(item)
--- church_website/urls.py
+++ church_website/urls.py
@@ -1,29 +1,39 @@
+from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticViewSitemap
-from .views import home, church_news_archive, reading_page, youth_church_landing, gallery, book, map, menfellow, wemenfellow, youth, child, podcast, game
+from .views import home, church_news_archive, reading_page, youth_church_landing, gallery, book, map, menfellow, wemenfellow, youth, child, podcast, game, youth_activities
 
+# Define your sitemaps
 sitemaps = {
     "static": StaticViewSitemap,
 }
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("subscribe/", include("newsletter.urls")),
-    path("", home, name="home"),
-    path("church-news-archive/", church_news_archive, name="church_news_archive"),
-    path("youth-church-landing/", youth_church_landing, name="youth_church_landing"),
-    path("gallery/", gallery, name="gallery"),
-    path("map/", map, name="map"),
-    path("menfellow/", menfellow, name="menfellow"),
-    path("wemenfellow/", wemenfellow, name="wemenfellow"),
-    path("youth/", youth, name="youth"),
-    path("book/", book, name="book"),
-    path("child/", child, name="child"),
-    path("reading/", reading_page, name="reading_page"),
-    path("podcast/", podcast, name="podcast"),
-    path("game/", game, name="game"),
-
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+    path("subscribe/", include("newsletter.urls")),  
+    path("", home, name="home"), 
+    path('church-news-archive/', church_news_archive, name='church_news_archive'),
+    path('youth-church-landing/', youth_church_landing, name='youth_church_landing'),
+    path('gallery/', gallery, name='gallery'),
+    path('map/', map, name='map'),
+    path('menfellow/', menfellow, name='menfellow'),
+    path('wemenfellow/', wemenfellow, name='wemenfellow'),
+    path('youth/', youth, name='youth'),
+    path('youth-activities/', youth_activities, name='youth_activities'),
+    path('book/', book, name='book'),
+    path('child/', child, name='child'),
+    path('reading/', reading_page, name='reading_page'),
+    path('podcast/', podcast, name='podcast'),
+    path('game/', game, name='game'),
+    
+    # Sitemap URL
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
