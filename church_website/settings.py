1936f3d020c0617159e8c1ae7d0aa91e2e52a89a
qwame2 <papaqamwe25@gmail.com>
2026-03-10 10:11:44 +0000

    Updates

--- church_website/settings.py
+++ church_website/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-for-dev-only')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "thecocsm.org", "www.thecocsm.org", "31.25.239.229"]
 
 # Application definition
 INSTALLED_APPS = [
